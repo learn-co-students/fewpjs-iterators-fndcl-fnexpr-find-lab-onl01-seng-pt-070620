12bdf17035aa678adb4b1cc78e8746cd4a6c3c4f
Cristian Deleon <libaration@gmail.com>
2021-01-10 16:41:55 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (records) => {
+  let winner = records.find(record => {
+    return record.result === 'W'
+  })
+  return !!winner ? winner.year : undefined
+}
