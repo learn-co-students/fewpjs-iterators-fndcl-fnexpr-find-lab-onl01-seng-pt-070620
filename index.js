9c5f8bec58a313af361502abbe5aa5271e26b7b0
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-01-25 10:57:49 -0800

    Done.

--- index.js
+++ index.js
@@ -4,26 +4,13 @@ function testFunc() {
   return "hi"
 }
 
-// superbowlWin = () => record.find(function win(obj) {
-//   if (obj.result === 'W')
-//     return obj.year
-// })
-
-// attempt 2
-let winResult = record.find(data => data.result === "W");
-
-function superbowlWin() {
-  return winResult.year
+function superbowlWin(record) {
+  let result = record.find(function(data) {
+    return data.result === "W"
+  });
+  if (result)
+    return result.year
+  else 
+    return undefined
 }
 
-
-// solution 2 using iteration
-// function search(nameKey, myArray){
-//   for (var i=0; i < myArray.length; i++) {
-//       if (myArray[i].name === nameKey) {
-//           return myArray[i];
-//       }
-//   }
-// }
-
-// var win = search("W", record);
