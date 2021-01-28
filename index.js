6d92ef75377a3e75fabc0b080544641147e51aa6
katiethile <lethikatie@gmail.com>
2021-01-27 21:11:31 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin (record) {
+const result = record.find( r => r.result === "W")
+return (!!result) ? result.year : undefined
 }
