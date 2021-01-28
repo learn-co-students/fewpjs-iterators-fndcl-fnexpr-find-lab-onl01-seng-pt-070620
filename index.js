78be91d283c8f30ca22be02e30ae10ef0d044c7a
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-01-28 07:30:17 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(record){
+  const r =  record.find(function(r){
+    return r.result === "W";
+  });
+  return r ? r.year : undefined;
+}
