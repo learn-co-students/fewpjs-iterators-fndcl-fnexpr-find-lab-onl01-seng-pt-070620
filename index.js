dad386e41a3e30461ec6a7043b501a8fabe2929d
kam3r0nz <kam.hungerford@gmail.com>
2021-01-28 19:50:57 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(records) {
+  records.forEach(record => function(record) {
+    record.find(function(s) { if (s.result === "W") { return s.year } else { return undefined }})
+  })
 }
