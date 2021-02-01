31e9de60cdab49f8e172185034f6360259c2bd5d
gracenak <gracenak@gmail.com>
2021-02-01 15:47:34 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const record = {}
+
+function superbowlWin(record) {
+  let result = record.find(record => 
+    record.result === "W")
+  if (result) {
+  return result.year
+  } else
+  undefined
+}
+
