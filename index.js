80e4ca785d01992a909a4322071a307fccc0faa0
Srog1989 <s.rogers0715@yahoo.com>
2021-02-01 19:28:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+const superbowl = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+superbowlWin = (superbowl) => {
+  let result = superbowl.find( superbowl => superbowl.result === "W" )
+  return !!result ? result.year : undefined
 }
