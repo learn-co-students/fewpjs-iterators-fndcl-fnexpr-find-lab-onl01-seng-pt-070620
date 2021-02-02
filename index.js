40c19445f9366e8cb659783f0289b76a64b6f7a1
Justin Lupica <justinlupica@gmail.com>
2021-02-02 14:47:35 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = {}
+
+function superbowlWin(record) {
+  let result = record.find(record => 
+    record.result === "W")
+  if (result) {
+  return result.year
+  } else
+  undefined
+}
